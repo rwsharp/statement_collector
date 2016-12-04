591a8ee71cec63e733b579d00b18656147026d95
Richard Sharp <rsharp@gmail.com>
2016-12-04 11:09:15 -0800

    added logging, updated comments

--- main.py
+++ main.py
@@ -1,36 +1,63 @@
-import json
-import tweepy
+"""Gather tweets from the public API and build up a history."""
+
 import sys
 import os.path
-import argparse
-import datetime
 import shutil
+import datetime
+import argparse
+import logging
+import json
+
+import tweepy
 
 
-def observed_tweets(data_file_name):
-    # get the set of tweets already in the data file to avoid duplicates
-    tweet_ids = set()
-    if os.path.isfile(data_file_name):
-        with open(data_file_name, 'r') as data_file:
-            for line in data_file:
-                status = json.loads(line)
-                tweet_ids.add(status['id'])
+def get_config(config_file_name):
+    """Gather the twitter connection configuration details.
+
+    :param config_file_name: name of json file with twitter connection details
+    :return: configuration dictionary
+    """
+    if os.path.isfile(config_file_name):
+        with open(config_file_name, 'r') as config_file:
+            config = json.load(config_file)
     else:
-        print 'WARNING - Did not find an existing data file. Will create new file: ' + str(data_file_name)
+        message = 'ERROR - config file not found:' + str(config_file_name)
+        logging.error(message)
+        raise ValueError(message)
 
-    return tweet_ids
+    return config
+
+
+def get_args():
+    """Build arg parser and get command line arguments
+
+    :return: parsed args namespace
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--config-file", default="config/config.json", help="config file name")
+    parser.add_argument("--data-path", default="data", help="path to data file")
+    parser.add_argument("--data-file", default="trump_dump.json", help="data file name")
+    parser.add_argument("--backup-suffix", default=".bak",
+                        help="suffix appended to data file name to create backup data file")
+    parser.add_argument("--query", default="from:realDonaldTrump", help="data file name")
+
+    args = parser.parse_args()
+
+    return args
 
 
 def main(args):
+    """Execute the Twitter query and append results to the history file.
+
+    :param args: command line arguments from argparse
+    :return: exit status
+    """
 
     # load config file with Twitter account details
-    if os.path.isfile(args.config_file):
-        with open(args.config_file, 'r') as config_file:
-            config = json.load(config_file)
-    else:
-        raise ValueError('ERROR - config file not found:' + str(args.config_file))
+    config = get_config(args.config_file)
 
-    # check the data path and file
+    # check the validity of the data path and file
     data_path = args.data_path
     data_file_name = os.path.join(data_path, args.data_file)
 
@@ -40,24 +67,36 @@ def main(args):
             os.makedirs(args.data_path)
         else:
             # path exists, but it's not a directory
-            raise ValueError('ERROR - data path (' + str(args.data_path) + ')already exists, but it is not a directory.')
+            message = 'ERROR - data path (' + str(args.data_path) + ')already exists, but is not a directory.'
+            logging.error(message)
+            raise ValueError(message)
 
     # create backup of old file if it exists
     if os.path.isfile(data_file_name):
-        backup_file_name = data_file_name + '.bak'
+        backup_file_name = data_file_name + args.backup_suffix
         shutil.copy(data_file_name, backup_file_name)
 
-    # get the latest tweets and add them to the data file
-    # some tweets may be duplicates for ones recorded earlier, however, some data such as retweet count may be updated
-    # giving some ability to see if the short term popularity of a tweet spikes
+    # get the latest tweets
     auth = tweepy.OAuthHandler(config['consumer']['key'], config['consumer']['secret'])
     auth.set_access_token(config['access']['token'], config['access']['secret'])
 
     api = tweepy.API(auth)
     max_tweets = 1000
     query = args.query
-    search_result = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
 
+    try:
+        logging.info('executing query')
+        search_result = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
+    except:
+        logging.error('ERROR - something went wrong with the query.')
+        raise
+
+    # Append the tweets to the data file.
+    #
+    # Some tweets may be duplicates of ones recorded earlier, however, some fields such as retweet count may have
+    # updated values. By recording the tweet again there will be some ability to see if its short term popularity has
+    # spiked, the tweet has been deleted, etc.
+    logging.info('appening query results to history file')
     with open(data_file_name, 'a') as data_file:
         for status in search_result:
             tweet = {'collected at': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
@@ -66,15 +105,29 @@ def main(args):
                      'status': status._json}
             print >> data_file, json.dumps(tweet)
 
+    return 0
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    logging.basicConfig(filename='statement_collector.log', filemode='a',
+                        format='%(asctime)s %(filename)s:%(funcName)s:%(levelname)s:%(message)s', level=logging.INFO)
+    logging.info('started')
 
-    parser.add_argument("--config-file", default="config/config.json",     help="config file name")
-    parser.add_argument("--data-path",   default="data",            help="path to data file")
-    parser.add_argument("--data-file",   default="trump_dump.json", help="data file name")
-    parser.add_argument("--query",       default="from:realDonaldTrump", help="data file name")
+    try:
+        args = get_args()
+    except:
+        logging.error('failed to parse command line arguments')
+        logging.error('failure')
+        logging.info('finished')
+        raise
 
-    args = parser.parse_args()
+    try:
+        main(args)
+        logging.info('success')
+        logging.info('finished')
+    except:
+        logging.error('failure')
+        logging.info('finished')
+        raise
 
-    main(args)
+
