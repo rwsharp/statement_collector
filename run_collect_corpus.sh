ea883113a362e722d3777c26cb0b37c7822a14e1
Ubuntu <ubuntu@ip-172-31-7-72.us-east-2.compute.internal>
2017-07-22 01:01:12 +0000

    new paths for new system

--- run_collect_corpus.sh
+++ run_collect_corpus.sh
@@ -3,18 +3,18 @@
 
 DATE=`date +%Y%m%d`
 
-WP_PATH=/home/bitnami/apps/wordpress/htdocs/wp-content/uploads/trump
-DATA_PATH=/home/bitnami/data/trump
+BACKUP_PATH=/home/ubuntu/backup/data
+DATA_PATH=/home/ubuntu/data/
 
 OWNER_SLUG="cspan:members-of-congress"
 DATA_FILE="list_timeline_cspan_members_of_congress.json"
 OUTPUT_PATH=${DATA_PATH}/cspan_members_tweets
 
-cd /home/bitnami/repos/statement_collector
+cd /home/ubuntu/repos/statement_collector
 
-COMMAND="/home/bitnami/ve_p2.7.12/bin/python /home/bitnami/repos/statement_collector/collect_corpus_from_list.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --list-owner-slug \"${OWNER_SLUG}\""
+COMMAND="/home/ubuntu/ve_p2.7.12/bin/python /home/ubuntu/repos/statement_collector/collect_corpus_from_list.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --list-owner-slug \"${OWNER_SLUG}\""
 eval ${COMMAND}
 
-COMMAND="/home/bitnami/ve_p2.7.12/bin/python /home/bitnami/repos/statement_collector/prepare_corpus.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --output-path ${OUTPUT_PATH}"
+COMMAND="/home/ubuntu/ve_p2.7.12/bin/python /home/ubuntu/repos/statement_collector/prepare_corpus.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --output-path ${OUTPUT_PATH}"
 eval ${COMMAND}
 
--- run_statement_collector.sh
+++ run_statement_collector.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 set -x
 
-WP_PATH=/home/bitnami/apps/wordpress/htdocs/wp-content/uploads/trump
-DATA_PATH=/home/bitnami/data/trump
+BACKUP_PATH=/home/ubuntu/backup/data
+DATA_PATH=/home/ubuntu/data
 
 QUERY="from:realDonaldTrump"
 DATA_FILE=trump_dump.json
 
-cd /home/bitnami/repos/statement_collector
-/home/bitnami/ve_p2.7.12/bin/python /home/bitnami/repos/statement_collector/main.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --query "${QUERY}"
-cp ${DATA_PATH}/${DATA_FILE} ${WP_PATH}/${DATA_FILE}
+cd /home/ubuntu/repos/statement_collector
+/home/ubuntu/ve_p2.7.12/bin/python /home/ubuntu/repos/statement_collector/main.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --query "${QUERY}"
+cp ${DATA_PATH}/${DATA_FILE} ${BACKUP_PATH}/${DATA_FILE}
 
 
 QUERY="from:POTUS"
 SINCE_ID="822446982648210000"  # Between the last Obama @POTUS tweet: 822446982648201200 and the first Trump @POTUS tweet: 822521968465416200 
 DATA_FILE=potus45.json
 
-cd /home/bitnami/repos/statement_collector
-/home/bitnami/ve_p2.7.12/bin/python /home/bitnami/repos/statement_collector/main.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --query "${QUERY}" --since-id ${SINCE_ID}
-cp ${DATA_PATH}/${DATA_FILE} ${WP_PATH}/${DATA_FILE}
+cd /home/ubuntu/repos/statement_collector
+/home/ubuntu/ve_p2.7.12/bin/python /home/ubuntu/repos/statement_collector/main.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --query "${QUERY}" --since-id ${SINCE_ID}
+cp ${DATA_PATH}/${DATA_FILE} ${BACKUP_PATH}/${DATA_FILE}
 
 
 QUERY="from:SenWarren OR from:elizabethforma"
 DATA_FILE=elizabeth_warren.json
 
-cd /home/bitnami/repos/statement_collector
-/home/bitnami/ve_p2.7.12/bin/python /home/bitnami/repos/statement_collector/main.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --query "${QUERY}"
-cp ${DATA_PATH}/${DATA_FILE} ${WP_PATH}/${DATA_FILE}
+cd /home/ubuntu/repos/statement_collector
+/home/ubuntu/ve_p2.7.12/bin/python /home/ubuntu/repos/statement_collector/main.py --data-path ${DATA_PATH} --data-file ${DATA_FILE} --query "${QUERY}"
+cp ${DATA_PATH}/${DATA_FILE} ${BACKUP_PATH}/${DATA_FILE}
 
